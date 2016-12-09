fa3f09eb24851ee72c894bd6597dedc9d88be8e0
Kajetan Lipensky <kajetanlipensky@gmail.com>
2016-12-09 19:27:03 +0100

    Added dataloader for mocking data

--- Cocoamphoactate/controllers/RecommendationController.py
+++ Cocoamphoactate/controllers/RecommendationController.py
@@ -1,18 +1,14 @@
-from django.http import *
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view, authentication_classes
-from rest_framework.exceptions import ParseError
 from rest_framework.response import Response
 
-from ..recommendation_engine.recommendations import Engine, ALL_USERS, FRIENDS_ONLY
-from ..serializers import *
+from ..recommendation_engine.recommendations import Engine
 
 
 class RecommendationController():
     @api_view(['GET'])
-    @authentication_classes((TokenAuthentication, ))
+    @authentication_classes((TokenAuthentication,))
     def get(request, pk, t):
         engine = Engine()
         try:
@@ -20,8 +16,7 @@ class RecommendationController():
             engine.set_type(t)
         except ValueError as e:
             return Response(str(e), status=status.HTTP_400_BAD_REQUEST)
-        return Response({'detail': 'GET answer', 'user' : pk, 'type' : t}, status=status.HTTP_200_OK)
-
+        return Response({'detail': 'GET answer', 'user': pk, 'type': t}, status=status.HTTP_200_OK)
 
     @api_view(['GET'])
     @authentication_classes((TokenAuthentication,))
