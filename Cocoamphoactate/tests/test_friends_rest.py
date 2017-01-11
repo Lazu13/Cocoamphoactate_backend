e730b9f94a9cd4a5a7dbbe05e5d654fb184c4a47
Kajetan Lipensky <kajetanlipensky@gmail.com>
2017-01-11 11:33:02 +0100

    test 4

---
+++ Cocoamphoactate/tests/test_friends_rest.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from django.contrib.auth.models import User
+from django.test import Client
+from django.test import TestCase
+from rest_framework.authtoken.models import Token
+
+from Cocoamphoactate.models import FriendsPending, Friends
+
+
+class FriendsPendingRestTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        User(id=1, username="restUser1", password="restPassword1").save()
+        User(id=2, username="restUser2", password="restPassword2").save()
+        Token(user_id=1, created=datetime.now(), key="testToken1").save()
+        Token(user_id=2, created=datetime.now(), key="testToken2").save()
+        Friends(user_one_id=1, user_two_id=2).save()
+
+    def test_should_get_all_my_friends(self):
+        response = self.client.get("/friends/my", **{'HTTP_AUTHORIZATION': 'Token testToken1'})
+        self.assertEqual(response.json()[0]["username"], "restUser2")
